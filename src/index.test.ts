11e24ce47a146d59f809cfaac35353546386cee3
Mara Kim <mara@autochthe.net>
2025-03-29 04:35:15 -0700

    organize

--- src/index.test.ts
+++ src/index.test.ts
@@ -45,28 +45,6 @@ const hashIndex = createValkeyIndex(
   },
 );
 
-const relationIndex = createValkeyIndex(
-  {
-    valkey,
-    name: "relation",
-    related: relatedHash<TestObject, "bar" | "baz">({ fields: ["bar", "baz"] }),
-    get: getHash(),
-    set: setHash(),
-    update: updateHash(),
-  },
-  {
-    use: async ({ get, update }, pkey: string) => {
-      const val = await get!(pkey);
-      if (val?.baz === undefined) {
-        return;
-      }
-      const next = val.baz + 1;
-      await update!({ pkey, input: { baz: next } });
-      return next;
-    },
-  },
-);
-
 test("Hash index", async () => {
   expect(await hashIndex.get("1")).toEqual({});
 
@@ -94,6 +72,28 @@ test("Hash index", async () => {
   expect(await hashIndex.f.use("1")).toEqual(1);
   expect(await hashIndex.get("1")).toEqual({ foo: "lalala", bar: 0, baz: 1 });
 });
+
+const relationIndex = createValkeyIndex(
+  {
+    valkey,
+    name: "relation",
+    related: relatedHash<TestObject, "bar" | "baz">({ fields: ["bar", "baz"] }),
+    get: getHash(),
+    set: setHash(),
+    update: updateHash(),
+  },
+  {
+    use: async ({ get, update }, pkey: string) => {
+      const val = await get!(pkey);
+      if (val?.baz === undefined) {
+        return;
+      }
+      const next = val.baz + 1;
+      await update!({ pkey, input: { baz: next } });
+      return next;
+    },
+  },
+);
 
 test("Relation index", async () => {
   expect(await relationIndex.get("1")).toEqual({});
