0719134905ed034938e6886ed8e48616cc5ddfe5
Mara Kim <mara@autochthe.net>
2025-04-09 12:11:05 -0700

    fine grained control

--- src/type.ts
+++ src/type.ts
@@ -3,76 +3,80 @@ import SuperJSON from "superjson";
 const DEFAULT_FROM_STRING = SuperJSON.parse;
 const DEFAULT_TO_STRING = SuperJSON.stringify;
 
-export type ValkeyType<T> = {
-  fromString: (input: string) => T | undefined;
-  fromStringMap: (input: Record<string, string | undefined>) => Partial<T>;
-  toString: (input: T) => string;
-  toStringMap: (
-    input: Partial<T>,
-  ) => Record<string, string | undefined> | undefined;
+export type Constructable<T> = ((input: string) => T | undefined) & {
+  toString?: () => string;
 };
 
-export type FromString<T> = (
-  input: string,
-) => (T & { toString?: () => string }) | undefined;
+export type FromString<T> = (input: string) => T | undefined;
 export type ToString<T> = (input: T) => string;
 
-export type FromStringMap<T> = { [R in keyof T]: FromString<T[R]> };
-export type ToStringMap<T> = { [R in keyof T]: ToString<T[R]> };
+export type FromStringMap<T> = (
+  input: Record<string, string | undefined>,
+) => Partial<T> | undefined;
+export type ToStringMap<T> = (
+  input: T,
+) => Record<string, string | undefined> | undefined;
 
-export function ValkeyType<T>(
-  fromString?: FromString<T> | FromStringMap<T>,
-  toString?: ToString<T> | ToStringMap<T>,
-): ValkeyType<T> {
-  return {
-    fromString:
-      fromString instanceof Function
-        ? fromString ?? DEFAULT_FROM_STRING
-        : DEFAULT_FROM_STRING,
-    fromStringMap: deserializeRecord(
-      fromString && !(fromString instanceof Function)
-        ? fromString
-        : ({} as FromStringMap<T>),
-    ),
-    toString:
-      toString instanceof Function
-        ? toString ?? DEFAULT_TO_STRING
-        : DEFAULT_TO_STRING,
-    toStringMap: serializeRecord(
-      toString && !(toString instanceof Function)
-        ? toString
-        : ({} as ToStringMap<T>),
-    ),
-  };
+export type ValkeyType<T> = {
+  fromString: FromString<T>;
+  fromStringMap: FromStringMap<T>;
+  toString: ToString<T>;
+  toStringMap: ToStringMap<T>;
+};
+
+export type ValkeyTypeProps<T> =
+  | {
+      fromString?: FromString<T>;
+      fromStringMap?: FromStringMap<T>;
+      toString?: ToString<T>;
+      toStringMap?: ToStringMap<T>;
+    }
+  | Constructable<T>;
+
+export function ValkeyType<T>(props?: ValkeyTypeProps<T>): ValkeyType<T> {
+  if (props === undefined) {
+    return {
+      fromString: DEFAULT_FROM_STRING,
+      fromStringMap: deserializeRecord(),
+      toString: DEFAULT_TO_STRING,
+      toStringMap: serializeRecord(),
+    };
+  } else if (props instanceof Function) {
+    return {
+      fromString: props,
+      fromStringMap: deserializeRecord(),
+      toString: props.toString ? props.toString : DEFAULT_TO_STRING,
+      toStringMap: serializeRecord(),
+    };
+  } else {
+    return {
+      fromString: props.fromString ?? DEFAULT_FROM_STRING,
+      fromStringMap: deserializeRecord(),
+      toString: props.toString ?? DEFAULT_TO_STRING,
+      toStringMap: serializeRecord(),
+    };
+  }
 }
 
-function serializeRecord<T>(record: ToStringMap<T>) {
+function serializeRecord() {
   return function serialize<T>(input: T) {
     if (!input) {
       return {};
     } else if (typeof input === "object") {
       return Object.fromEntries(
         Object.entries(input).map(([key, val]) => {
-          return [
-            key,
-            (record[key as keyof typeof record] ?? DEFAULT_TO_STRING)(val),
-          ];
+          return [key, DEFAULT_TO_STRING(val)];
         }),
       );
     }
   };
 }
 
-function deserializeRecord<T>(record: FromStringMap<T>) {
+function deserializeRecord() {
   return function deserialize<T>(input: Record<string, string | undefined>) {
     return Object.fromEntries(
       Object.entries(input).map(([key, val]) => {
-        return [
-          key,
-          val
-            ? (record[key as keyof typeof record] ?? DEFAULT_FROM_STRING)(val)
-            : val,
-        ];
+        return [key, val ? DEFAULT_FROM_STRING(val) : val];
       }),
     ) as Partial<T>;
   };
