e1aa8377b4334e5cdadaffbdd589a66d61b772ea
Mara Kim <mara@autochthe.net>
2025-04-14 03:35:57 -0700

    pipeline exec

--- src/pipeline.ts
+++ src/pipeline.ts
@@ -6,17 +6,38 @@ export type ValkeyPipelineProps = {
 
 export type ValkeyPipelineAction<T> = (
   pipeline: ChainableCommander,
-) => (results: [error: Error | null, result: unknown][]) => T;
+) => ValkeyPipelineGetter<T>;
+
+export type ValkeyPipelineGetter<T> = (
+  results: [error: Error | null, result: unknown][],
+) => T;
+
+type ValkeyPipelineGetters = Record<string, ValkeyPipelineGetter<any>[]>;
 
 export function ValkeyPipeline({ pipeline }: ValkeyPipelineProps) {
-  const getters = {};
+  const getters: ValkeyPipelineGetters = {};
 
   function add<T>(label: string, action: ValkeyPipelineAction<T>) {
     const getter = action(pipeline);
+    if (label in getters) {
+      getters.label?.push(getter);
+    } else {
+      getters[label] = [getter];
+    }
   }
 
-  async function exec() {
+  async function exec<T extends Record<string, any[]>>() {
     const results = await pipeline.exec();
+    if (!results) {
+      return null;
+    }
+    const values: Record<string, any[]> = {};
+    Object.entries(getters).forEach(([label, list]) => {
+      values[label] = list.map((getter) => {
+        getter(results);
+      });
+    });
+    return values as T;
   }
 
   return {
