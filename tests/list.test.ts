eb6b80067730e52b3c68b13f2b2e550b6bb7be1d
Mara Kim <mara@autochthe.net>
2025-04-15 01:26:45 -0700

    add pipeline tests

--- tests/list.test.ts
+++ tests/list.test.ts
@@ -1,5 +1,6 @@
 import { ValkeyType, ValkeyListIndex } from "../src";
 import { useBeforeEach, valkey, type TestObject } from ".";
+import { ValkeyPipeline } from "../src/pipeline";
 
 useBeforeEach();
 
@@ -83,4 +84,31 @@ test("List", async () => {
   expect(await listIndex.index({ pkey: 1, index: 1 })).toEqual(null);
   expect(await listIndex.index({ pkey: 1, index: 2 })).toEqual(null);
   expect(await listIndex.len({ pkey: 1 })).toEqual(1);
+});
+
+test("List pipeline", async () => {
+  const pipeline1 = ValkeyPipeline(valkey);
+  pipeline1.add("index", listIndex.pipe.index({ pkey: 1, index: 0 }));
+  pipeline1.add("index", listIndex.pipe.index({ pkey: 1, index: 1 }));
+  pipeline1.add("index", listIndex.pipe.index({ pkey: 1, index: 2 }));
+  pipeline1.add("len", listIndex.pipe.len({ pkey: 1 }));
+  expect(await pipeline1.exec()).toEqual({
+    index: [null, null, null],
+    len: [0],
+  });
+
+  const pipeline2 = ValkeyPipeline(valkey);
+  pipeline2.add(
+    "push",
+    listIndex.pipe.push({ pkey: 1, input: { foo: "ababa", bar: 0 } }),
+  );
+  pipeline2.add("index", listIndex.pipe.index({ pkey: 1, index: 0 }));
+  pipeline2.add("index", listIndex.pipe.index({ pkey: 1, index: 1 }));
+  pipeline2.add("index", listIndex.pipe.index({ pkey: 1, index: 2 }));
+  pipeline2.add("len", listIndex.pipe.len({ pkey: 1 }));
+  expect(await pipeline2.exec()).toEqual({
+    push: [1],
+    index: [{ foo: "ababa", bar: 0 }, null, null],
+    len: [1],
+  });
 });
