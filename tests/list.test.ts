4552139d69d6bb56d8a03cc8953da59bc16a67ea
Mara Kim <mara@autochthe.net>
2025-04-18 22:23:22 -0700

    Add pipeline tests

--- tests/list.test.ts
+++ tests/list.test.ts
@@ -111,4 +111,108 @@ test("List pipeline", async () => {
     index: [{ foo: "ababa", bar: 0 }, null, null],
     len: [1],
   });
+
+  const pipeline3 = ValkeyPipeline(valkey);
+  pipeline3.add(
+    "push",
+    listIndex.pipe.push({ pkey: 1, input: { foo: "lalala", bar: 1 } }),
+  );
+  pipeline3.add("index", listIndex.pipe.index({ pkey: 1, index: 0 }));
+  pipeline3.add("index", listIndex.pipe.index({ pkey: 1, index: 1 }));
+  pipeline3.add("index", listIndex.pipe.index({ pkey: 1, index: 2 }));
+  pipeline3.add("len", listIndex.pipe.len({ pkey: 1 }));
+  expect(await pipeline3.exec()).toEqual({
+    push: [2],
+    index: [
+      {
+        foo: "lalala",
+        bar: 1,
+      },
+      {
+        foo: "ababa",
+        bar: 0,
+      },
+      null,
+    ],
+    len: [2],
+  });
+
+  const pipeline4 = ValkeyPipeline(valkey);
+  pipeline4.add(
+    "rpush",
+    listIndex.pipe.rpush({ pkey: 1, input: { foo: "gagaga", bar: 2 } }),
+  );
+  pipeline4.add("index", listIndex.pipe.index({ pkey: 1, index: 0 }));
+  pipeline4.add("index", listIndex.pipe.index({ pkey: 1, index: 1 }));
+  pipeline4.add("index", listIndex.pipe.index({ pkey: 1, index: 2 }));
+  pipeline4.add("len", listIndex.pipe.len({ pkey: 1 }));
+  expect(await pipeline4.exec()).toEqual({
+    rpush: [3],
+    index: [
+      {
+        foo: "lalala",
+        bar: 1,
+      },
+      {
+        foo: "ababa",
+        bar: 0,
+      },
+      {
+        foo: "gagaga",
+        bar: 2,
+      },
+    ],
+    len: [3],
+  });
+
+  const pipeline5 = ValkeyPipeline(valkey);
+  pipeline5.add("pop", listIndex.pipe.pop({ pkey: 1 }));
+  pipeline5.add("index", listIndex.pipe.index({ pkey: 1, index: 0 }));
+  pipeline5.add("index", listIndex.pipe.index({ pkey: 1, index: 1 }));
+  pipeline5.add("index", listIndex.pipe.index({ pkey: 1, index: 2 }));
+  pipeline5.add("len", listIndex.pipe.len({ pkey: 1 }));
+  expect(await pipeline5.exec()).toEqual({
+    pop: [
+      {
+        foo: "lalala",
+        bar: 1,
+      },
+    ],
+    index: [
+      {
+        foo: "ababa",
+        bar: 0,
+      },
+      {
+        foo: "gagaga",
+        bar: 2,
+      },
+      null,
+    ],
+    len: [2],
+  });
+
+  const pipeline6 = ValkeyPipeline(valkey);
+  pipeline6.add("rpop", listIndex.pipe.rpop({ pkey: 1 }));
+  pipeline6.add("index", listIndex.pipe.index({ pkey: 1, index: 0 }));
+  pipeline6.add("index", listIndex.pipe.index({ pkey: 1, index: 1 }));
+  pipeline6.add("index", listIndex.pipe.index({ pkey: 1, index: 2 }));
+  pipeline6.add("len", listIndex.pipe.len({ pkey: 1 }));
+  expect(await pipeline6.exec()).toEqual({
+    rpop: [
+      {
+        foo: "gagaga",
+        bar: 2,
+      },
+    ],
+    index: [
+      {
+        foo: "ababa",
+        bar: 0,
+      },
+      null,
+      null,
+    ],
+    len: [1],
+  });
 });
